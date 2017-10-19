ad1d3f91fdcbc8ef5dae210108c80df8e4f30ed2
chentetran <vincenttran23@gmail.com>
2017-10-18 22:37:32 -0400

    move firebase set up to new file

---
+++ HSHSmobile/config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  FIREBASE_API_KEY: "AIzaSyBv7bMYBUsPHR1MRvnSuhkeZ3HHqmvcp60",
+  FIREBASE_AUTH_DOMAIN: "hshs-street-team.firebaseapp.com",
+  FIREBASE_DATABASE_URL: "https://hshs-street-team.firebaseio.com",
+  FIREBASE_STORAGE_BUCKET: "hshs-street-team.appspot.com"
+}
