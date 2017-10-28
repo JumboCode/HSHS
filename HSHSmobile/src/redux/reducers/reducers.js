6505b170a5439fa6317228e600e0673574a40a80
chentetran <vincenttran23@gmail.com>
2017-10-28 13:36:23 -0400

    Added getGuests action +reducer

--- HSHSmobile/src/redux/reducers/reducers.js
+++ HSHSmobile/src/redux/reducers/reducers.js
@@ -1,14 +1,10 @@
 import { combineReducers } from 'redux'
-<<<<<<< 6a58d935c2a01e1557be760e1a668bd5e359c167
 import { firebaseStateReducer as firebase } from 'react-redux-firebase'
 <<<<<<< c0d22032d398db94e40c500d080619c90335e525
 const rootReducer = combineReducers({
   firebase
 })
 export default rootReducer
-=======
-=======
->>>>>>> Firebase info in action
 
 export function reducer(state, action) {
     switch (action.type) {
@@ -17,19 +13,26 @@ export function reducer(state, action) {
                 loading: true
             });
 
-        case 'GET_MURALS_SUCCESS':
+        case 'GET_INFO_SUCCESS':
         	return Object.assign({}, state, {
         		loading: false,
         		loaded: true,
         		info: action.payload
         	});
 
-        // TODO: more reducers go here
+        case 'GET_GUESTS_START':
+            return Object.assign({}, state, {
+                loading: true
+            });
+
+        case 'GET_GUESTS_SUCCESS':
+            return Object.assign({}, state, {
+                loading: false,
+                loaded: true,
+                guests: action.payload
+            });
 
         default:
             return state;
     }
 }
-
-//export default rootReducer
->>>>>>> store is undefined
