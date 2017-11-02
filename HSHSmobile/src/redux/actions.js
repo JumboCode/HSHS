caa1506b0c2f74ae30ec8580730ea4bd08781ece
sakeerthy <sakeerthy@gmail.com>
2017-11-01 21:41:29 -0400

    added actions and reducers

--- HSHSmobile/src/redux/actions.js
+++ HSHSmobile/src/redux/actions.js
@@ -21,3 +21,20 @@ export const getGuests = () => {
 					}, 0);
 				});
 }
+
+export const addNewGuestStart = () => ({
+	type: 'ADD_NEW_GUEST_START'
+});
+
+export const addNewGuestSuccess = () => ({
+	type: 'ADD_NEW_GUEST_SUCCESS'
+})
+
+export const addNewGuest = (name, age, gender, notes) => {
+	firebase.database().ref('demo/guests' + userId).set({
+    		name: name,
+    		age: age,
+    		gender : gender, 
+    		notes: notes
+  });
+}
