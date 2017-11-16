15bb39c6c352640655d305d40957d05d5a9f97da
Jacob Jaffe <jacob.jaffe@tufts.edu>
2017-11-15 21:49:01 -0500

    guests sorted

--- HSHSmobile/src/GuestList.js
+++ HSHSmobile/src/GuestList.js
@@ -40,6 +40,20 @@ function guestObjectToArray(IdsToGuests, IdsToInteractions) {
         });
     }
 
+    guestList.sort((a, b) => {
+        var nameA = a.name.toUpperCase(); // ignore upper and lowercase
+        var nameB = b.name.toUpperCase(); // ignore upper and lowercase
+        if (nameA < nameB) {
+            return -1;
+        }
+        if (nameA > nameB) {
+            return 1;
+        }
+
+        // names must be equal
+        return 0;
+    });
+
     return guestList;
 }
 
