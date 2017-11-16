4fb7d9bbc6c8f0b641a34d935a2ccea6a9f2f732
jgrace03 <jusugrace@gmail.com>
2017-11-16 18:16:30 -0500

    added icons to login

--- HSHSmobile/App.js
+++ HSHSmobile/App.js
@@ -3,106 +3,31 @@ import {Provider} from 'react-redux';
 import {store} from './src/redux/store.js';
 import GuestList from './src/GuestList';
 import Dashboard from './src/Dashboard';
+import Login from './src/Login';
 import Info from './src/Info';
 import GuestListProfile from './src/GuestListProfile';
 import CRUDnote from './src/CRUDnote';
 import GuestListNew from './src/GuestListNew';
 
-export default  () => {
+export default () => {
     return new App()
 };
 
-const Icon = require('react-native-vector-icons/Ionicons');
-
-var homeIcon // ios-home-outline
-var todolistIcon // ios-paper-outline
-var checkinIcon // ios-list-outline
-var guestsIcon // ios-people-outline
-var rescourcesIcon // ios-help-circle-outline
-var addIcon // ios-add-circle-outline
-
 class App {
   constructor() {
-
-    this._populateIcons().then(() => {
-      // Start app only if all icons are loaded
-      this.startApp();
-    }).catch((error) => {
-      console.error(error);
-    });
+    this.startApp();
   }
 
-
-  _populateIcons = function () {
-    return new Promise(function (resolve, reject) {
-      Promise.all(
-        [
-            Icon.getImageSource('ios-home-outline', 30),
-            Icon.getImageSource('ios-list-outline', 30),
-            Icon.getImageSource('ios-paper-outline', 30),
-            Icon.getImageSource('ios-people-outline', 30),
-            Icon.getImageSource('ios-help-circle-outline', 30),
-            Icon.getImageSource('ios-add-circle-outline', 30)
-        ]
-      ).then((values) => {
-        homeIcon = values[0];
-        todolistIcon = values[1];
-        checkinIcon = values[2];
-        guestsIcon = values[3];
-        rescourcesIcon = values[4];
-        addIcon = values[5];
-        resolve(true);
-      }).catch((error) => {
-        console.log(error);
-        reject(error);
-      }).done();
-    });
-  };
-
     startApp() {
-    // this will start our app
-    Navigation.registerComponent('Dashboard', () => Dashboard, store, Provider);
-    Navigation.registerComponent('GuestList', () => GuestList, store, Provider);
-    Navigation.registerComponent('GuestListProfile', () => GuestListProfile, store, Provider);
-    Navigation.registerComponent('Info', () => Info, store, Provider);
-    Navigation.registerComponent('CRUDnote', () => CRUDnote, store, Provider);
-    Navigation.registerComponent('GuestListNew', () => GuestListNew, store, Provider);
+        // this will start our app
+        Navigation.registerComponent('Login', () => Login, store, Provider);
 
-    // TODO: make the tabs link to real pages
-    Navigation.startTabBasedApp({
-        tabs: [
-            {
-                label:'dashboard',
-                screen: 'Dashboard',
-                title: 'Dashboard',
-                icon: homeIcon
-
-            },
-            {
-                label: 'todo_list',
-                screen: 'Info',
-                title: 'Todo List',
-                icon: todolistIcon
-            },
-            {
-                label: 'check_in',
-                screen: 'Info',
-                title: 'Check In',
-                icon: checkinIcon
-            },
-            {
-                label: 'guests',
-                screen: 'GuestList',
-                title: 'Guests',
-                icon: guestsIcon,
-            },
-            {
-                label: 'rescources',
-                screen: 'Info',
-                title: 'Rescources',
-                icon: rescourcesIcon
-            }
-        ]
-    })
+        Navigation.startSingleScreenApp({
+            screen: {
+                screen: 'Login',
+                //title: 'Dashboard',
+                navigatorStyle: { navBarHidden: true },
+             },
+        });
     }
 }
---
+++ HSHSmobile/src/Login.js
@@ -0,0 +1,219 @@
+import React, { Component } from 'react';
+import {store} from './redux/store.js';
+import {Provider} from 'react-redux';
+import {Navigation} from 'react-native-navigation';
+import {
+    Platform,
+    StyleSheet,
+    Text,
+    View,
+    FlatList,
+    Button,
+    ActivityIndicator,
+    TextInput
+} from 'react-native';
+import { List, ListItem, SearchBar } from "react-native-elements";
+
+import GuestList from './GuestList';
+import Dashboard from './Dashboard';
+import Info from './Info';
+import GuestListNew from './GuestListNew';
+import GuestListProfile from './GuestListProfile';
+
+import Icon from 'react-native-vector-icons/Ionicons';
+
+const Icons = require('react-native-vector-icons/Ionicons');
+
+var homeIcon // ios-home-outline
+var todolistIcon // ios-paper-outline
+var checkinIcon // ios-list-outline
+var guestsIcon // ios-people-outline
+var rescourcesIcon // ios-help-circle-outline
+var addIcon // ios-add-circle-outline
+
+var mailIcon
+var lockIcon
+
+export default class Login extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { text: 'Useless Placeholder' };
+    }
+
+    openApp () {
+        console.log("open");
+        this._populateIcons().then(() => {
+        // Start app only if all icons are loaded
+            this.startApp();
+        }).catch((error) => {
+            console.error(error);
+        });
+    };
+
+    authenticate = () => {
+        this.openApp();
+    };
+
+    forgotPassword = () => {
+
+    };
+
+    _populateIcons = function () {
+        return new Promise(function (resolve, reject) {
+          Promise.all(
+            [
+                Icons.getImageSource('ios-home-outline', 30),
+                Icons.getImageSource('ios-list-outline', 30),
+                Icons.getImageSource('ios-paper-outline', 30),
+                Icons.getImageSource('ios-people-outline', 30),
+                Icons.getImageSource('ios-help-circle-outline', 30),
+                Icons.getImageSource('ios-add-circle-outline', 30),
+                Icons.getImageSource('ios-mail-outline', 30),
+                Icons.getImageSource('ios-lock-outline', 30),
+            ]
+          ).then((values) => {
+            homeIcon = values[0];
+            todolistIcon = values[1];
+            checkinIcon = values[2];
+            guestsIcon = values[3];
+            rescourcesIcon = values[4];
+            addIcon = values[5];
+            mailIcon = values[6];
+            lockIcon = values[7];
+            resolve(true);
+          }).catch((error) => {
+            console.log(error);
+            reject(error);
+          }).done();
+        });
+    };
+
+    startApp() {
+        // this will start our app
+        Navigation.registerComponent('Dashboard', () => Dashboard, store, Provider);
+        Navigation.registerComponent('GuestList', () => GuestList, store, Provider);
+        Navigation.registerComponent('GuestListProfile', () => GuestListProfile, store, Provider);
+        Navigation.registerComponent('Info', () => Info, store, Provider);
+        Navigation.registerComponent('CRUDnote', () => CRUDnote, store, Provider);
+        Navigation.registerComponent('GuestListNew', () => GuestListNew, store, Provider);
+
+        // TODO: make the tabs link to real pages
+        Navigation.startTabBasedApp({
+            tabs: [
+                {
+                    label:'dashboard',
+                    screen: 'Dashboard',
+                    title: 'Dashboard',
+                    icon: homeIcon
+
+                },
+                {
+                    label: 'todo_list',
+                    screen: 'Info',
+                    title: 'Todo List',
+                    icon: todolistIcon
+                },
+                {
+                    label: 'check_in',
+                    screen: 'Info',
+                    title: 'Check In',
+                    icon: checkinIcon
+                },
+                {
+                    label: 'guests',
+                    screen: 'GuestList',
+                    title: 'Guests',
+                    icon: guestsIcon,
+                },
+                {
+                    label: 'rescources',
+                    screen: 'Info',
+                    title: 'Rescources',
+                    icon: rescourcesIcon
+                }
+            ]
+        })
+    }    
+
+    render() {
+        //console.log(this.props.data);
+        return (
+            <View style = {styles.container}>
+            <Text style = {styles.title}> Harvard Square </Text>
+            <Text style = {styles.title}> Homeless Shelter </Text>
+            <Text style = {styles.title}> Volunteer App </Text>
+
+            <View style = {styles.row} >
+                <Icon style = {styles.icon} 
+                      name="ios-mail-outline" 
+                      size={25} 
+                      color="#FFFFFF" 
+                />
+                <TextInput 
+                    style={styles.textInput} 
+                    placeholder='Username' 
+                    inlineImageLeft='ios-person-add'
+                />
+            </View>
+            <View style = {styles.row} >
+            <Icon style = {styles.icon}
+                    name="ios-lock-outline"
+                    size={25} color="#FFFFFF" />
+            <TextInput style={styles.textInput} placeholder='Password' />
+            </View>
+            <View style = {styles.login}>
+                 <Button
+                  onPress={this.authenticate}
+                  title="Login"
+                  color="#FFFFFF"
+                  accessibilityLabel="Learn more about this purple button"
+                />
+            </View>
+           <Text onPress={this.forgotPassword} style = {styles.forgotPassword}> 
+                  Forgot your password?
+            </Text>
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#770B16',
+    },
+    row: {
+        flexDirection: "row",
+    },
+    login : {
+        //flex: 1,
+        width: 275,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#556A5B',
+    },
+    title : {
+        justifyContent: 'center',
+        alignItems: 'center',
+        fontSize: 25,
+        color: "#FFFFFF",
+    },
+    forgotPassword : {
+        fontSize: 12,
+        color: "#FFFFFF",
+    },
+    textInput: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        color: "#FFFFFF",
+        height: 30,
+        width: 300,
+        fontSize: 15,
+        backgroundColor: '#770B16',
+    },
+    icon: {
+        paddingRight: 10
+    }
+});
