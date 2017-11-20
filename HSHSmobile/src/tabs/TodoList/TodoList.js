a96d36ca42535571dcd8023cb951bebecae4e352
Jacob Jaffe <jacob.jaffe@tufts.edu>
2017-11-20 16:47:23 -0500

    renamed TodoList class

--- HSHSmobile/src/tabs/TodoList/TodoList.js
+++ HSHSmobile/src/tabs/TodoList/TodoList.js
@@ -12,7 +12,7 @@ import {
     View
 } from 'react-native';
 
-export default class Info extends Component {
+export default class TodoList extends Component {
     render() {
         return (
             <View style={styles.container}>
