0f1ec4226df8e9798e86a3c360d8fef7489b5769
Jacob Jaffe <jacob.jaffe@tufts.edu>
2017-12-01 20:06:52 -0500

    action items render on map

--- HSHSmobile/src/tabs/Dashboard/Dashboard.js
+++ HSHSmobile/src/tabs/Dashboard/Dashboard.js
@@ -17,8 +17,12 @@ import {getGuests, getInteractions, getActionItems} from '../../redux/actions.js
 const Icon = require('react-native-vector-icons/Ionicons');
 
 function mapStateToProps(state, ownProps) {
-    return {};
-}
+    return {
+        actionItems: state.actionItems,
+        guests: state.guests,
+        loading: state.loading,
+        interactions: state.interactions
+    };}
 
 function mapDispatchToProps(dispatch, ownProps) {
     return {
@@ -32,13 +36,7 @@ class Dashboard extends Component {
     constructor(props) {
         super(props);
         this.props.navigator.setOnNavigatorEvent(this.onNavigatorEvent.bind(this));
-
-        this.state = {
-            page: 1,
-            seed: 1,
-            error: null,
-            refreshing: false
-        };
+        this.props.loading = true;
     };
 
     onNavigatorEvent(event) { // this is the onPress handler for the two buttons together
@@ -102,71 +100,91 @@ class Dashboard extends Component {
         //return <SearchBar placeholder="Type Here..." lightTheme round />;
     };
 
-    renderFooter = () => {
-        if (!this.state.loading) return null;
+    renderMarkers = () => {
 
-        return (
-            <View
-                style={{
-                    paddingVertical: 20,
-                    borderTopWidth: 0,
-                    borderColor: "#CED0CE"
-                }}
-            >
-                <ActivityIndicator animating size="large" />
-            </View>
-        );
+        var markers = [];
+        for (var actionItemId in this.props.actionItems) {
+            let actionItem = this.props.actionItems[actionItemId];
+            let coordinate = {latitude: actionItem.locationCoord.lat, longitude: actionItem.locationCoord.lng};
+            markers.push(
+                <MapView.Marker
+                    coordinate = {coordinate}
+                    title = {actionItem.title}
+                    description = {actionItem.description}
+                    key = {actionItemId}
+                />
+            )
+        }
+        return markers;
     };
 
+    // I'm not sure if this is the best way to have logical statements within renders, but it's not the worst way!
     render() {
-        console.log(this.props.data);
         return (
-<View>
-      <MapView
-        style={{height: Dimensions.get('window').height*0.4, width: Dimensions.get('window').width, margin: 0}}
-    initialRegion={{
-      latitude: 42.405804,
-      longitude: -71.11956,
-      latitudeDelta: 0.02,
-      longitudeDelta: 0.01,
-    }}>
-    </MapView>
-    <View style={{flexDirection: 'row', alignSelf: 'center'}}>
-        <Button
-        onPress={() => {}}
-  title="Add New Note"
-  color="#841584"
-  accessibilityLabel="Learn more about this purple button"
-/>
-<Button
-  onPress={() => {}}
-  title="Add Action Item"
-  color="#841584"
-  accessibilityLabel="Learn more about this purple button"
-/>
-    </View>
-    <View style={{flexDirection: 'column', alignItems: 'center'}}>
-        <Text>
-            Lottery Status: Number not given out
-        </Text>
-        <View style={{flexDirection: 'row'}}>
-            <View style={{width: 50, height: 50, backgroundColor: 'powderblue', borderWidth: 1, borderColor: 'black', alignItems: 'center', justifyContent: 'center'}}><Text>0</Text></View>
-            <View style={{width: 50, height: 50, backgroundColor: 'powderblue', borderWidth: 1, borderColor: 'black', alignItems: 'center', justifyContent: 'center'}}><Text>0</Text></View>
-            <View style={{width: 50, height: 50, backgroundColor: 'powderblue', borderWidth: 1, borderColor: 'black', alignItems: 'center', justifyContent: 'center'}}><Text>0</Text></View>
-        </View>
-        <View style={{flexDirection: 'row'}}>
-            <View style={{width: 50, height: 50, backgroundColor: 'powderblue', borderWidth: 1, borderColor: 'black', alignItems: 'center', justifyContent: 'center'}}><Text>0</Text></View>
-            <View style={{width: 50, height: 50, backgroundColor: 'powderblue', borderWidth: 1, borderColor: 'black', alignItems: 'center', justifyContent: 'center'}}><Text>0</Text></View>
-            <View style={{width: 50, height: 50, backgroundColor: 'powderblue', borderWidth: 1, borderColor: 'black', alignItems: 'center', justifyContent: 'center'}}><Text>0</Text></View>
-        </View>
-            <Button
-                onPress={() => {}}
-                title="Call on Lottery"
-                color="#841584"
-                accessibilityLabel="Learn more about this purple button"
-            />
-    </View>
-    </View> 
+
+            (this.props.loading &&  <ActivityIndicator animating size="large" />) ||
+
+
+            (!this.props.loading &&
+            <View>
+                <MapView
+                    style = {{
+                        height: Dimensions.get('window').height*0.4,
+                        width: Dimensions.get('window').width,
+                        margin: 0}}
+                    initialRegion={{
+                        latitude: 42.405804,
+                        longitude: -71.11956,
+                        latitudeDelta: 0.02,
+                        longitudeDelta: 0.01,
+                    }}>
+                    {
+                        this.props.actionItems && this.renderMarkers()
+                    }
+                </MapView>
+
+                <View style={{flexDirection: 'row', alignSelf: 'center'}}>
+                    <Button
+                    onPress={() => {}}
+                    title="Add New Note"
+                    color="#841584"
+                    accessibilityLabel="Learn more about this purple button"
+                    />
+                    <Button
+                        onPress={() => {}}
+                        title="Add Action Item"
+                        color="#841584"
+                        accessibilityLabel="Learn more about this purple button"
+                    />
+                </View>
+
+                <View style={{flexDirection: 'column', alignItems: 'center'}}>
+
+                    <Text>
+                        Lottery Status: Number not given out
+                    </Text>
+
+                    <View style={{flexDirection: 'row'}}>
+                        <View style={{width: 50, height: 50, backgroundColor: 'powderblue', borderWidth: 1, borderColor: 'black', alignItems: 'center', justifyContent: 'center'}}><Text>0</Text></View>
+                        <View style={{width: 50, height: 50, backgroundColor: 'powderblue', borderWidth: 1, borderColor: 'black', alignItems: 'center', justifyContent: 'center'}}><Text>0</Text></View>
+                        <View style={{width: 50, height: 50, backgroundColor: 'powderblue', borderWidth: 1, borderColor: 'black', alignItems: 'center', justifyContent: 'center'}}><Text>0</Text></View>
+                    </View>
+
+                    <View style={{flexDirection: 'row'}}>
+                        <View style={{width: 50, height: 50, backgroundColor: 'powderblue', borderWidth: 1, borderColor: 'black', alignItems: 'center', justifyContent: 'center'}}><Text>0</Text></View>
+                        <View style={{width: 50, height: 50, backgroundColor: 'powderblue', borderWidth: 1, borderColor: 'black', alignItems: 'center', justifyContent: 'center'}}><Text>0</Text></View>
+                        <View style={{width: 50, height: 50, backgroundColor: 'powderblue', borderWidth: 1, borderColor: 'black', alignItems: 'center', justifyContent: 'center'}}><Text>0</Text></View>
+                    </View>
+
+                    <Button
+                        onPress={() => {}}
+                        title="Call on Lottery"
+                        color="#841584"
+                        accessibilityLabel="Learn more about this purple button"
+                    />
+                </View>
+            </View>
+            )
         );
     }
 }
