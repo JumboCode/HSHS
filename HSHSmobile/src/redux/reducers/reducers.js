a728863a08d39e9c6eb941fd30e3f1cb00d9f0d9
pjain03 <pulkit.jain@tufts.edu>
2017-12-16 13:56:47 -0500

    - styled - edited reducers to figure out receptivity

---
+++ HSHSmobile/src/redux/reducers/reducers.js
@@ -0,0 +1,65 @@
+import { combineReducers } from 'redux'
+import { firebaseStateReducer as firebase } from 'react-redux-firebase'
+const rootReducer = combineReducers({
+  firebase
+});
+export default rootReducer
+
+export function reducer(state, action) {
+    switch (action.type) {
+
+        case 'GET_GUESTS_START':
+            return Object.assign({}, state, {
+                loading: true
+            });
+
+        case 'GET_GUESTS_SUCCESS':
+            return Object.assign({}, state, {
+                loading: false,
+                loaded: true,
+                guests: action.payload
+            });
+            
+        case 'GET_INTERACTIONS_START':
+            return Object.assign({}, state, {
+                loading: true
+            });
+
+        case 'GET_INTERACTIONS_SUCCESS':
+            return Object.assign({}, state, {
+                loading: false,
+                loaded: true,
+                interactions: action.payload
+            });
+        case 'ADD_GUEST_START':
+            return Object.assign({}, state, {
+                adding: true
+            });
+        case 'ADD_NEW_GUEST_SUCCESS':
+            return Object.assign({}, state, {
+                adding: false
+            });
+        case 'ADD_NEW_ACTION_ITEM_START':
+            return Object.assign({}, state, {
+                addingAction: true
+            });
+        case 'ADD_NEW_ACTION_ITEM_SUCCESS':
+            return Object.assign({}, state, {
+                addingAction: false
+            });
+        case 'GET_ACTION_ITEMS_START':
+            return Object.assign({}, state, {
+                loading: true
+            });
+
+        case 'GET_ACTION_ITEMS_SUCCESS':
+            return Object.assign({}, state, {
+                loading: false,
+                loaded: true,
+                actionItems: action.payload
+            });
+        
+        default:
+            return state;
+    }
+}
---
+++ HSHSmobile/src/tabs/GuestList/GuestListProfile.js
@@ -0,0 +1,219 @@
+/* GuestProfile */
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+    StyleSheet,
+    Text,
+    View,
+    Image,
+    FlatList,
+} from 'react-native';
+import { List, ListItem } from "react-native-elements";
+import nodeEmoji from 'node-emoji';
+import {connect} from 'react-redux';
+
+const Timestamp = require('react-timestamp');
+
+function mapStateToProps(state, ownProps) {
+    return {
+        guest: state.guests[ownProps.Id],
+        loading: state.loading,
+        testing: state
+    };
+}
+
+
+class GuestProfile extends Component {
+    constructor(props) {
+        super(props);
+        // this.props.navigator.setOnNavigatorEvent(this.onNavigatorEvent.bind(this));
+        this.view_crud_note_page = this.view_crud_note_page.bind(this);
+        console.log(this.props.testing);
+    };
+
+    // matching receptivity to emojis {0-4} where 4 is the best and 0 is the worst
+    id_to_emoji = [":smirk:", ":slightly_smiling_face:", ":grinning:", ":smiley:", ":smile:"];
+
+    /********************** Helper functions section **********************/
+
+    //
+    view_crud_note_page = () => {
+      this.props.navigator.push({
+          screen: 'CRUDnote', // unique ID registered with Navigation.registerScreen
+          passProps: {
+              name: this.props.guest.name
+          }, // Object that will be passed as props to the pushed screen (optional)
+          animated: true, // does the push have transition animation or does it happen immediately (optional)
+          animationType: 'fade', // ‘fade’ (for both) / ‘slide-horizontal’ (for android) does the push have different transition animation (optional)
+          backButtonHidden: false, // hide the back button altogether (optional)
+          navigatorStyle: {}, // override the navigator style for the pushed screen (optional)
+          navigatorButtons: {} // override the nav buttons for the pushed screen (optional)
+      });
+    };
+    /********************** Render functions section **********************/
+
+    // renders name on profile page
+    render_name = () => {
+        return (
+            <Text style={styles.name}>
+                {this.props.guest.name}
+            </Text>
+        );
+    };
+
+    // renders age on profile page
+    render_age = () => {
+        return (
+            <Text style={styles.note}>
+                {this.props.guest.age}
+            </Text>
+        );
+    };
+
+    // renders gender on profile page
+    render_gender = () => {
+        return (
+            <Text style={styles.note}>
+                {this.props.guest.gender}
+            </Text>
+        );
+    };
+
+    // renders gender on profile page
+    render_hairColor = () => {
+        return (
+            <Text style={styles.note}>
+                {this.props.guest.hairColor}
+            </Text>
+        );
+    };
+
+    // renders gender on profile page
+    render_tattoo = () => {
+        return (
+            <Text>
+                {this.props.guest.tattoo}
+            </Text>
+        );
+    };
+
+    // renders last interacted
+    render_interacted = () => {
+        return (
+            <Text style={styles.last_interacted}>
+                Last Interacted: <Timestamp time={this.props.guest.last_interacted} component={Text}/>
+            </Text>
+        );
+    };
+
+    // render description
+    render_description = () => {
+        return (
+            <Text style={styles.note}>
+                {this.props.guest.description}
+            </Text>
+        );
+    };
+
+    // render notes
+    render_notes = () => {
+        let note_list = this.profile_data.notes;
+        if(note_list) {
+            return (
+                <View style={styles.note_section}>
+                    <Text style={styles.notes}>
+                        Notes:
+                    </Text>
+                    <FlatList
+                        data={note_list}
+                        renderItem={({ item }) => (
+                            <ListItem
+                                title={item.note}
+                                onPress={() => this.view_crud_note_page()}
+                            />
+                        )}
+                        style={styles.note}
+                        keyExtractor={item => item.note}
+                    />
+                </View>
+            );
+        }
+    };
+
+    /********************** Setup screen **********************/
+
+    render() {
+        return (
+            <View style={styles.container}>
+                <View style={styles.top}>
+                    <View style={styles.profile_info}>
+                        {this.render_name()}
+                        {this.render_age()}
+                        {this.render_gender()}
+                        {this.render_interacted()}
+                        {this.render_description()}
+                    </View>
+                </View>
+                {/*{this.render_notes()}*/}
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        flexDirection: 'column',
+        backgroundColor: 'white',
+    },
+    top: {
+        flex: 1,
+        justifyContent: "center",
+        flexDirection: "row",
+        flexWrap: "nowrap",
+        padding: 20,
+        borderWidth: 1,
+        borderColor: 'black',
+    },
+    note_section: {
+        flex: 5,
+        flexDirection: "column",
+        justifyContent: "flex-start",
+        alignItems: "flex-start",
+        margin: 10,
+        padding: 10,
+    },
+    note: {
+        flexDirection: 'row',
+    },
+    notes: {
+        fontSize: 22,
+    },
+    name: {
+        fontSize: 22,
+        textDecorationColor:'#686868',
+        fontFamily: 'Times New Roman',
+    },
+    profile_info: {
+        flex: 2,
+        justifyContent: 'flex-start',
+        alignItems: 'baseline',
+        flexDirection: 'column',
+        paddingLeft: 10,
+    },
+    instructions: {
+        textAlign: 'center',
+        color: '#333333',
+        margin: 10,
+    },
+    last_interacted: {
+        textDecorationStyle: '',
+    },
+});
+
+export default connect(mapStateToProps)(GuestProfile);
