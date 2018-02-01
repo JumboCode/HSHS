0e7f91aade3f5bbf7f27ead521bc601dc3d46bcc
chasetrox <chasetrox@gmail.com>
2018-02-01 16:23:52 -0500

    Created styles and circled text

--- HSHSmobile/src/modules/Counter.js
+++ HSHSmobile/src/modules/Counter.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react';
 import {
+  StyleSheet,
 	FlatList,
 	View,
 	Text,
-  Button
+  Button,
+  TouchableOpacity
 } from 'react-native';
 import { List, ListItem } from "react-native-elements";
-import { Icon } from 'react-native-elements'
+import Icon from 'react-native-vector-icons/Feather';
+
+const upArrowIcon = (<Icon name="chevron-thin-up" size={15} color="#900" />)
+const downArrowIcon = (<Icon name="chevron-thin-down" size={15} color="#900" />)
+
 
 // Props: item name, count
 export default class Counter extends Component {
@@ -16,20 +22,40 @@ export default class Counter extends Component {
 
   render() {
 		return (
-      <View style={{flex: 1, flexDirection: 'column',
-                    justifyContent: 'space-around',
+      <View style={{flexDirection: 'column',
+                    justifyContent: 'center',
                     alignItems:'center'}}>
-        <Button
+        <TouchableOpacity
           onPress={() => {this.props.count++;}}
-          title={"Up"}>
-        </Button>
-        <Text>{this.props.count.toString()}</Text>
-        <Button
-          title={"Down"}
-          onPress={() => {if (this.props.count > 0) this.props.count--;}}>
-        </Button>
-        <Text>{this.props.itemName}</Text>
+        >
+          <Icon name="chevron-up" size={30} color="#900" />
+        </TouchableOpacity>
+        <View style={styles.circle}>
+          <Text style={styles.text}>{this.props.count.toString()}</Text>
+        </View>
+        <TouchableOpacity
+          onPress={() => {if (this.props.count > 0) this.props.count--;}}
+        >
+          <Icon name="chevron-down" size={30} color="#900" />
+        </TouchableOpacity>
+        <Text style={styles.text}>{this.props.itemName}</Text>
       </View>
 		);
 	}
 }
+
+const styles = StyleSheet.create({
+    circle: {
+      flexDirection:'column',
+      justifyContent:'center',
+      alignItems:'center',
+      backgroundColor: 'gray',
+      width: 50,
+      height: 50,
+      borderRadius: 50/2,
+      borderWidth: 1.5,
+    },
+    text: {
+      fontSize: 15,
+    }
+});
