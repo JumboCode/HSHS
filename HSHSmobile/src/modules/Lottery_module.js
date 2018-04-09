22af6ee6f9191c1d4edd8d84fda7d46f706c3429
chentetran <vincenttran23@gmail.com>
2018-04-08 21:37:23 -0400

    before/after 9:30 logic

--- HSHSmobile/src/modules/Lottery_module.js
+++ HSHSmobile/src/modules/Lottery_module.js
@@ -8,15 +8,29 @@ import renderSeperator from "./UI/renderSeperator";
 import renderLoader from "./UI/renderLoader";
 import dupNavFix from "../dupNavFix";
 
+// The time from start of day until lottery release
+const lotteryReleaseTimeInMillis = 77400000;
+
 class Lottery_module extends Component {
 	constructor(props) {
 		super(props);
 	}
 
+	_showCountdown() {
+		let now = Date.now();
+		let releaseDate = new Date();
+		releaseDate.setHours(0);
+		releaseDate.setMinutes(0);
+		let releaseTimestamp = releaseDate.getTime() + lotteryReleaseTimeInMillis;
+
+		return releaseTimestamp - now;
+	}
+
 	render() {
 		return (
-			<View style={{backgroundColor: 'green'}}>
-
+			<View style={{backgroundColor: 'green', flex: 1}}>
+				<Text>Countdown until lottery:</Text>
+				<Text>{this._showCountdown()}</Text>
 			</View>
 		);
 	}
