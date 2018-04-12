4b07632d7c20d420cf8f1a5909e9a02917855e80
ysllab <scarletxuiam0917@gmail.com>
2018-04-12 08:35:54 -0400

    functional count down

--- HSHSmobile/src/modules/Lottery_module.js
+++ HSHSmobile/src/modules/Lottery_module.js
@@ -14,23 +14,57 @@ const lotteryReleaseTimeInMillis = 77400000;
 class Lottery_module extends Component {
 	constructor(props) {
 		super(props);
+    this.state = {timeRemaining: "", seconds: 0 };
 	}
 
-	_showCountdown() {
-		let now = Date.now();
+  componentWillMount() {
+    let secs = this.calcTimeRemaining();
+    this.setState({ seconds: secs });
+  }
+
+  componentDidMount() {
+      this.interval = setInterval(() => {
+        this.countDown();
+        console.log(this.state.seconds);
+      }, 1000);
+  };
+
+  componentWillUnmount() {
+      clearInterval(this.interval);
+  }
+
+  calcTimeRemaining = () => {
+    let now = new Date();
 		let releaseDate = new Date();
 		releaseDate.setHours(0);
 		releaseDate.setMinutes(0);
 		let releaseTimestamp = releaseDate.getTime() + lotteryReleaseTimeInMillis;
+    return ((releaseTimestamp - now) / 1000).toFixed(0);
+  }
 
-		return releaseTimestamp - now;
+  countDown = () => {
+      let secs = this.state.seconds - 1;
+      let timeLeft = this.milisecondsToTime(secs);
+      this.setState({timeRemaining: timeLeft, seconds: secs});
+  }
+
+	milisecondsToTime = (seconds) => {
+    let minutes = Math.floor(seconds / 60);
+    let hours = "0";
+    if (minutes > 59) {
+      hours = Math.floor(minutes / 60);
+      minutes = minutes - (hours * 60);
+      minutes = (minutes > 10) ? minutes : "0" + minutes;
+    }
+    seconds = seconds % 60;
+		return (hours >= 10 ? hours : ("0" + hours)) + ":" + minutes + ":" + ((seconds >= 10) ? seconds : "0" + seconds) ;
 	}
 
 	render() {
 		return (
 			<View style={{backgroundColor: 'green', flex: 1}}>
 				<Text>Countdown until lottery:</Text>
-				<Text>{this._showCountdown()}</Text>
+				<Text>{this.state.timeRemaining}</Text>
 			</View>
 		);
 	}
