d1b9e29b5b462b5645d749165586e10ffbf29bfd
chentetran <vincenttran23@gmail.com>
2018-05-01 02:38:25 -0400

    nav bar button to blck

--- HSHSmobile/src/tabs/ActionItems/ActionItem_view.js
+++ HSHSmobile/src/tabs/ActionItems/ActionItem_view.js
@@ -102,7 +102,7 @@ class ActionItem_view extends Component {
                   disabled: false, // optional, used to disable the button (appears faded and doesn't interact)
                   disableIconTint: true, // optional, by default the image colors are overridden and tinted to navBarButtonColor, set to true to keep the original image colors
                   showAsAction: 'ifRoom', // optional, Android only. Control how the button is displayed in the Toolbar. Accepted valued: 'ifRoom' (default) - Show this item as a button in an Action Bar if the system decides there is room for it. 'always' - Always show this item as a button in an Action Bar. 'withText' - When this item is in the action bar, always show it with a text label even if it also has an icon specified. 'never' - Never show this item as a button in an Action Bar.
-                  buttonColor: 'white', // Optional, iOS only. Set color for the button (can also be used in setButtons function to set different button style programatically)
+                  buttonColor: 'black', // Optional, iOS only. Set color for the button (can also be used in setButtons function to set different button style programatically)
                   buttonFontSize: 18, // Set font size for the button (can also be used in setButtons function to set different button style programatically)
                   buttonFontWeight: '600', // Set font weight for the button (can also be used in setButtons function to set different button style programatically)
               }
